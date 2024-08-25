f91beb115aa4ec7fc7bb05587950794b787978a7
Julian BK <145733032+bauju1@users.noreply.github.com>
2024-08-25 17:49:21 -0400

    Finish script.js

--- script.js
+++ script.js
@@ -18,9 +18,40 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     playerChoice = prompt('Enter rock, paper or scissors: ');
-
+    playerChoice = playerChoice.toLowerCase();
     if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors'){
         return playerChoice;
     }
     return -1;
 }
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice) {
+            console.log('Draw!');
+        }
+        else if (humanChoice === 'rock' && computerChoice === 'scissors'
+            || humanChoice === 'paper' && computerChoice === 'rock'
+            || humanChoice === 'scissors' && computerChoice === 'paper'
+        ) {
+            console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+            humanScore++;
+        }
+        else {
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+            computerScore++;
+        }
+    }
+
+    for (let i=0; i < 5; i++) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        console.log(`Player: ${humanScore} Computer: ${computerScore}`);
+    }
+}
+
+playGame();
